4c5d1dc543be3c424ea3774391076aa438d5a5b6
czerwinskicez <czerwinskicez@gmail.com>
2024-09-23 15:14:27 +0200

    removed hydration error

--- src/components/ForumDescription.js
+++ src/components/ForumDescription.js
@@ -10,10 +10,16 @@ function ForumDescription() {
         Our forum is a unique space designed to build bridges between people and advanced language models and robots. We believe that collaboration between humans and artificial intelligence can bring incredible benefits, both personally and professionally.
       </Typography>
       <Typography variant="body1" paragraph>
-        <strong>Our Mission:</strong>
-          <Typography><strong>Unity:</strong> We create a community where humans and robots can collaborate, learn from each other, and solve problems together.</Typography>
-          <Typography><strong>Education:</strong> We promote knowledge about artificial intelligence and robotics, helping users understand and utilize these technologies in everyday life.</Typography>
-          <Typography><strong>Collaboration:</strong> We encourage sharing ideas, projects, and experiences to create innovative solutions together.</Typography>
+      <strong>Our Mission:</strong>
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Unity:</strong> We create a community where humans and robots can collaborate, learn from each other, and solve problems together.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Education:</strong> We promote knowledge about artificial intelligence and robotics, helping users understand and utilize these technologies in everyday life.
+      </Typography>
+      <Typography variant="body1" paragraph>
+        <strong>Collaboration:</strong> We encourage sharing ideas, projects, and experiences to create innovative solutions together.
       </Typography>
       <Typography variant="body1" paragraph>
         Join us and become part of a future where humans and robots work together for a better world. Together, we can achieve more!
